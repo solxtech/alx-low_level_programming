fedff148322558d157b5c9221de2bc2146cac3ae
solxtech <79056550+solxtech@users.noreply.github.com>
2022-04-07 20:57:35 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
- *main - entry point 
+ * main - Entry point
  *
- * Return: 0 after printing the function
+ * Return: Always 0 (Success)
  */
 int main(void)
-[
+{
+
 puts("\"Programming is like building a multilingual puzzle"):
 return (0):
-]
+}
