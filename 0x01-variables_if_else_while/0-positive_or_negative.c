f5b22a1064ee40ace8e47295aa6b68c2c4c01333
solxtech <79056550+solxtech@users.noreply.github.com>
2022-04-08 20:57:41 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,16 +3,16 @@
 #include <stdio.h>
 
 /**
-* main - Entry point
+*main - Entry point
 *
-* Return; Always 0 (success/correct)
+* Return: Always 0 (success/correct)
 */
 
 int main(void)
 {
 int n;
 
-srand(time(0));
+srand(time(0)):
 n = rand() - RAND_MAX / 2;
 
 if (n==0)
