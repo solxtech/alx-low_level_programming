9d5d7d5bc15488431c6b3f5a0787465b127d5108
solxtech <nwudesolomon1@gmail.com>
2022-04-08 04:44:52 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -21,11 +21,11 @@ printf("%i is Zero\n", n);
 }
 else if (n<0)
 {
-printf("%i is negative\n");
+printf("%i is negative\n", n);
 }
 else
 {
-printf("%i is positive\n");
+printf("%i is positive\n", n);
 }
 
 return(0);
